e90dbd8fea49333deccf1d818560b831c8d7e231
gamma_sigma <lateo1234@gmail.com>
2024-12-31 16:47:37 +0100

    Add test for insertion of children and value move
    #### `tests\test_binary_tree.cpp`
    - This new test is useful for the "Guess The Animal" game, so that a leaf node can be transformed into a parent node with two children, and the right child can assume the value of the parent node.

--- tests/test_binary_tree.cpp
+++ tests/test_binary_tree.cpp
@@ -270,6 +270,8 @@ TEST(BinaryTreeIteratorTest, SetValue)
   it.setValue(42);
   EXPECT_NE(it.getValue(), 17);
   EXPECT_EQ(it.getValue(), 42);
+  it.setValue(17); // Restore to the original value
+  EXPECT_EQ(it.getValue(), 17);
 
   it.moveToLeftChild();
   EXPECT_EQ(it.getValue(), 14);
@@ -277,6 +279,8 @@ TEST(BinaryTreeIteratorTest, SetValue)
   it.setValue(99);
   EXPECT_NE(it.getValue(), 14);
   EXPECT_EQ(it.getValue(), 99);
+  it.setValue(14); // Restore to the original value
+  EXPECT_EQ(it.getValue(), 14);
 
   it.moveToRightChild();
   EXPECT_EQ(it.getValue(), 15);
@@ -284,6 +288,15 @@ TEST(BinaryTreeIteratorTest, SetValue)
   it.setValue(100);
   EXPECT_NE(it.getValue(), 15);
   EXPECT_EQ(it.getValue(), 100);
+  it.setValue(15); // Restore to the original value
+  EXPECT_EQ(it.getValue(), 15);
+
+  // Perform a level-order traversal to check if the values were correctly set
+  env->result.clear();
+  env->tree.levelOrderTraversal(env->getNodeValue);
+
+  std::vector<int> expected = { 17, 14, 26, 11, 15, 24, 27, 10, 12, 19, 25, 30, 8, 13, 23, 32 };
+  EXPECT_EQ(env->result, expected);
 }
 
 // Tests if the current node is a leaf node
@@ -349,6 +362,54 @@ TEST(BinaryTreeIteratorTest, CreateChildren)
   // Check if the children were removed
   EXPECT_FALSE(env->tree.search(5));
   EXPECT_FALSE(env->tree.search(9));
+}
+
+// Test insertion of children and move value of current node to right child
+TEST(BinaryTreeIteratorTest, CreateChildrenAndMoveValue)
+{
+  auto it = env->tree.getIterator();
+  it.moveToLeftChild(); // Move to node 14
+  it.moveToLeftChild(); // Move to node 11
+  it.moveToLeftChild(); // Move to node 10
+  it.moveToLeftChild(); // Move to node 8
+
+  // Create children for node 8. Left child will have value 5; right child will have value 8; current node (8) will be overwritten with value 7
+  EXPECT_TRUE(it.createChildren(5, it.getValue()));
+  it.setValue(7); // Replace value of node 8 (current node) with 7
+  EXPECT_EQ(it.getValue(), 7); // Check current node value
+  EXPECT_FALSE(it.isLeaf()); // Node 7 (formerly 8) is no longer a leaf
+
+  auto it_go_left = it; // Copy the iterator, so that we can proceed both with the left and right children
+  auto it_restore = it; // Copy the iterator to restore the former "8" node to its original value
+
+  it.moveToRightChild(); // Move to node 8
+  EXPECT_EQ(it.getValue(), 8); // Check right child value
+  EXPECT_TRUE(it.isLeaf()); // Node 8 is a leaf
+
+  it_go_left.moveToLeftChild(); // Move to node 5
+  EXPECT_EQ(it_go_left.getValue(), 5); // Check left child value
+  EXPECT_TRUE(it_go_left.isLeaf()); // Node 5 is a leaf
+
+  // Remove children
+  env->tree.remove(5);
+  env->tree.remove(8);
+  EXPECT_TRUE(it.isLeaf()); // Node 7 is a leaf again
+
+  // Check if the children were removed
+  EXPECT_FALSE(env->tree.search(5));
+  EXPECT_FALSE(env->tree.search(8));
+
+  // Restore the former "8" node to its original value
+  it_restore.setValue(8);
+  EXPECT_EQ(it_restore.getValue(), 8); // Check current node value
+  EXPECT_TRUE(it_restore.isLeaf()); // Node 8 is a leaf again
+
+  // Execute a level-order traversal to check that the tree structure is back to the original state
+  env->result.clear();
+  env->tree.levelOrderTraversal(env->getNodeValue);
+
+  std::vector<int> expected = { 17, 14, 26, 11, 15, 24, 27, 10, 12, 19, 25, 30, 8, 13, 23, 32 };
+  EXPECT_EQ(env->result, expected);
 }
 
 // // Defining a `main` function in the test source file is optional for the Google Test framework, because it provides
